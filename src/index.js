20b70e86e5e0fc5e33a700ef10d26fe77acb55ac
Andrew Schwartz <andrew.schwartz@brain-power.com>
2020-01-07 06:48:15 -0500

    Import index.js from Google Cloud Console

---
+++ src/index.js
@@ -0,0 +1,40 @@
+// Main event function handler
+exports.main = async (req, res) => {
+  const { body, query } = req;
+  console.log("Body", body);
+  console.log("Query", query);
+
+  const { command } = body;
+  let status;
+  let response;
+
+  if (command === "/spend") {
+    ({ status, response } = handleSpend(body));
+  } else {
+    res.status(400).send(`Command ${command} not recognized`);
+  }
+
+  res.status(status).send(response);
+};
+
+function handleSpend(body) {
+  const { text, user_name, user_id } = body;
+  const timestamp = (new Date()).getTime();
+  const { ok, data } = parseSpend(text);
+
+  if (!ok) { return { status: 400, response: "Invalid command format." }; }
+
+  const { amount, category } = data;
+  const conf = `Amount: ${amount}\nCategory: ${category}`;
+  console.log(conf);
+
+  return {status: 200, response: conf};
+}
+
+function parseSpend(text) {
+  const pattern = /\$?(\d+(?:\.\d{1,2})?)\s+(?:on\s+)?(.+)/;
+  const match = text.match(pattern);
+  if (!match) { return {ok: false} };
+  const [,amount,category] = text.match(pattern);
+  return {ok: true, data: {amount, category}};
+}

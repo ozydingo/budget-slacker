4bd2dd92de57f79f8cd0fa1066a7aaa2ddd93a94
Andrew Schwartz <andrew.schwartz@brain-power.com>
2020-01-08 05:54:55 -0500

    Fix google_client.js to get a working, authorized client

--- src/google_client.js
+++ src/google_client.js
@@ -1,4 +1,6 @@
 const fs = require('fs');
+const util = require('util');
+
 const {google} = require('googleapis');
 
 const readFile = util.promisify(fs.readFile);
@@ -7,15 +9,15 @@ const CREDENTIALS_PATH  = 'credentials.json';
 const TOKEN_PATH = 'token.json';
 
 async function getAuthorizedClient() {
-  const creds = await readFile(CREDENTIALS_PATH);
+  const credentials = JSON.parse(await readFile(CREDENTIALS_PATH));
   //TODO: remove redirect uris?
-  const { client_secret, client_id, redirect_uris } = JSON.parse(creds).installed;
+  const { client_secret, client_id, redirect_uris } = credentials.installed;
   const client = new google.auth.OAuth2(
       client_id, client_secret, redirect_uris[0]
   );
 
-  const tokenData = await readFile(TOKEN_PATH);
-  client.setCredentials(JSON.parse(tokenData));
+  const token = JSON.parse(await readFile(TOKEN_PATH));
+  client.setCredentials(token);
 
   return client;
 }

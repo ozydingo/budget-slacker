c2bf802b0a244f6774737f2c43ee17a779378bba
Andrew Schwartz <andrew.schwartz@brain-power.com>
2020-02-13 04:50:14 -0500

    Add error response to slack

--- app/budget_worker/index.js
+++ app/budget_worker/index.js
@@ -1,3 +1,4 @@
+const Slack = require("./slack");
 const Spend = require("./spend");
 
 // (pubSubEvent, context)
@@ -12,6 +13,17 @@ exports.main = async (pubSubEvent) => {
   console.log("Got message:", data);
 
   const { expense, slackMessage } = data;
-  const { ok, error } = await Spend.handleSpend({ expense, slackMessage });
+  const { response_url } = slackMessage;
+  const { ok, error } = await Spend.handleSpend(
+    { expense, slackMessage }
+  ).catch(err => {
+    Slack.respond({
+      response_url,
+      text: "Oh no! Something went wrong when adding this expense to the spreadsheet."
+    }).then(response => {
+      console.log("Error message response:", response.status);
+    })
+    return {ok: false, error: err.message}
+  });
   console.log({ ok, error });
 };

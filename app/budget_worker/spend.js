7e37cb13a504d4dc77768ead157279f5e5f970bf
Andrew Schwartz <andrew.schwartz@brain-power.com>
2020-02-15 15:01:56 -0500

    Extract getBudgetInfo into a function for reuse by other commands

--- app/budget_worker/spend.js
+++ app/budget_worker/spend.js
@@ -15,36 +15,32 @@ const app_credentials_promise = accessSecret(
   "projects/526411321629/secrets/sheets-api-credentials/versions/1"
 ).then(JSON.parse);
 
-async function bail(message, promises = []) {
-  console.log("Bailing out with message:", message);
-  await Promise.all(promises);
-  return {
-    ok: false,
-    error: message
-  };
+async function getBudgetInfo(team_id) {
+  console.log("Fetching budget info");
+  const budget = await Budget.find(team_id);
+  if (!budget) { throw Error(`Budget not found for team ${team_id}!`); }
+
+  const { access_token, refresh_token, spreadsheet_id } = budget.data();
+  if (!access_token) { throw Error(`access_token is missing for team ${team_id}!`); }
+  if (!refresh_token) { throw Error(`refresh_token is missing for team ${team_id}!`); }
+  if (!spreadsheet_id) { throw Error(`spreadsheet_id is missing for team ${team_id}!`); }
+  console.log("Spreadsheet:", spreadsheet_id);
+  const token_credentials = { access_token, refresh_token };
+  const app_credentials = await app_credentials_promise;
+
+  return { spreadsheet_id, app_credentials, token_credentials };
 }
 
 async function handleSpend({ response_url, data }) {
   const { team_id } = data;
   const timestamp = new Date();
 
-  console.log("Fetching budget info");
-  const budget = await Budget.find(team_id);
-  if (!budget) { return bail("Unfortunately, I can't find this workspace's buduget."); }
-
-  const { access_token, refresh_token, spreadsheet_id } = budget.data();
-  if (!access_token) { return bail("access_token is missing from this workspace!"); }
-  if (!refresh_token) { return bail("refresh_token is missing from this workspace!"); }
-  if (!spreadsheet_id) { return bail("spreadsheet_id is missing from this workspace!"); }
-  console.log("Spreadsheet:", spreadsheet_id);
-
-  const token_credentials = { access_token, refresh_token };
-  const app_credentials = await app_credentials_promise;
+  const { spreadsheet_id, app_credentials, token_credentials } = await getBudgetInfo(team_id);
   const sheets = new Sheets(app_credentials, token_credentials);
 
   const { user_name, user_id, amount, category, note } = data;
 
-  // Prioritize fase response: get totals and send confirmation first
+  // Prioritize fast response: get totals and send confirmation first
   // Use await to avoid race between getTotals and addExpense
   const totals = await sheets.getTotals(spreadsheet_id);
   const totalForCategory = totals[category];

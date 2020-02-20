c0ca04b058af5b976f5f0c6292c977a6c6ca6ebf
Andrew Schwartz <andrew.schwartz@brain-power.com>
2020-02-20 05:47:43 -0500

    Break add_expense and get_totals into separate functions

---
+++ app/add_expense/index.js
@@ -0,0 +1,57 @@
+const { google } = require("googleapis");
+
+const EXPENSE_RANGE = "expenses!A1:F1";
+
+function epochToDatetime(timestamp) {
+  const month = timestamp.getMonth() + 1;
+  const date = timestamp.getDate();
+  const year = timestamp.getFullYear();
+  const hour = timestamp.getHours();
+  const minute = timestamp.getMinutes();
+  const second =  timestamp.getSeconds();
+  return  `${month}/${date}/${year} ${hour}:${minute}:${second}`;
+}
+
+function sheetClient({app_credentials, tokens}) {
+  const { client_id, client_secret } = app_credentials;
+
+  const client = new google.auth.OAuth2(
+    client_id,
+    client_secret,
+  );
+  client.setCredentials(tokens);
+
+  const sheets = google.sheets({version: "v4", auth: client});
+  return sheets;
+}
+
+async function addExpense({sheets, spreadsheet_id, expense}) {
+  const values = [
+    epochToDatetime(expense.timestamp),
+    expense.user_id,
+    expense.user_name,
+    expense.amount,
+    expense.category,
+    expense.note,
+  ];
+
+  console.log("Appending row with", values);
+  return sheets.spreadsheets.values.append({
+    spreadsheet_id,
+    range: EXPENSE_RANGE,
+    valueInputOption: "USER_ENTERED",
+    requestBody: { values: [values] },
+  });
+}
+
+async function main(req, res) {
+  const { app_credentials, tokens, spreadsheet_id } = req.body;
+  const sheets = sheetClient({app_credentials, tokens});
+  const {expense} = body;
+  await addExpense({sheets, spreadsheet_id, expense});
+  res.status(200).send({ok: true});
+}
+
+module.exports = {
+  main
+};
--- app/get_totals/index.js
+++ app/get_totals/index.js
@@ -1,17 +1,6 @@
 const { google } = require("googleapis");
 
-const EXPENSE_RANGE = "expenses!A1:F1";
 const HISTORY = 6;
-
-function epochToDatetime(timestamp) {
-  const month = timestamp.getMonth() + 1;
-  const date = timestamp.getDate();
-  const year = timestamp.getFullYear();
-  const hour = timestamp.getHours();
-  const minute = timestamp.getMinutes();
-  const second =  timestamp.getSeconds();
-  return  `${month}/${date}/${year} ${hour}:${minute}:${second}`;
-}
 
 function sheetClient({app_credentials, tokens}) {
   const { client_id, client_secret } = app_credentials;
@@ -39,41 +28,11 @@ async function getTotals({sheets, spreadsheet_id}) {
   return totals
 }
 
-async function addExpense({sheets, spreadsheetId, expense}) {
-  const values = [
-    epochToDatetime(expense.timestamp),
-    expense.user_id,
-    expense.user_name,
-    expense.amount,
-    expense.category,
-    expense.note,
-  ];
-
-  console.log("Appending row with", values);
-  return sheets.spreadsheets.values.append({
-    spreadsheetId,
-    range: EXPENSE_RANGE,
-    valueInputOption: "USER_ENTERED",
-    requestBody: { values: [values] },
-  });
-}
-
 async function main(req, res) {
-  const { action, app_credentials, tokens, spreadsheet_id } = req.body;
+  const { app_credentials, tokens, spreadsheet_id } = req.body;
   const sheets = sheetClient({app_credentials, tokens});
-  if (action === "get_totals") {
-    const totals = await getTotals({sheets, spreadsheet_id});
-    res.status(200).send(JSON.stringify(totals));
-  } else if (actiono === "add_expense") {
-    const {expense} = body;
-    await addExpense({sheets, spreadsheetId, expense});
-    res.status(200).send({ok: true});
-  } else {
-    const msg = `Unknoown action ${action}`;
-    console.error(msg);
-    res.status(400).send(msg);
-    return;
-  }
+  const totals = await getTotals({sheets, spreadsheet_id});
+  res.status(200).send(JSON.stringify(totals));
 }
 
 

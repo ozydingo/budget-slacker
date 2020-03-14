36285b29fde2e55f3d2bdf58f7314110a5a7a292
Andrew Schwartz <andrew.schwartz@brain-power.com>
2020-03-14 17:22:47 -0400

    (pubsub) Get app credentials from secrets instead of teamInfo

--- app/functions/handle_pubsub_message/index.js
+++ app/functions/handle_pubsub_message/index.js
@@ -1,7 +1,11 @@
 const axios = require("axios");
 
+const { getSecret } = require("./getSecret");
 const { invokeFunction } = require("./invoke_function.js");
 const responses = require("./responses.js");
+
+// Do this on function initializaion; it doesn't change.
+const credentialsPromise = getSecret(process.env.appCredentialsSecret);
 
 function messageSlack({response_url, data}) {
   console.log("Responding to Slack");
@@ -14,6 +18,14 @@ function messageSlack({response_url, data}) {
   });
 }
 
+function haveValidTokens(teamInfo) {
+  return teamInfo && teamInfo.tokens;
+}
+
+function haveValidSpreadsheet(teamInfo) {
+  return teamInfo && teamInfo.spreadsheet_id;
+}
+
 function getTeamInfo(team_id) {
   console.log(`Getting team info for ${team_id}`);
   return invokeFunction(process.env.teamsUrl, {action: "get", team_id});
@@ -22,22 +34,40 @@ function getTeamInfo(team_id) {
 async function router({ command, data, response_url }) {
   const teamInfo = await getTeamInfo(data.team_id);
 
-  if (!teamInfo || data.team_id === "TEST-OAUTH") {
+  if (!haveValidTokens(teamInfo)) {
     const oauthUrl = `${process.env.requestOauthUrl}?team_id=${encodeURIComponent(data.team_id)}`;
     const oauthMessage = responses.requestOauthBlocks({oauthUrl});
     return messageSlack({response_url, data: oauthMessage});
+  } else if (!haveValidSpreadsheet) {
+    return messageSlack({response_url, data: "Uh oh! I can't find your budget spreadsheet. Please contact support."});
   }
 
+  const app_credentials = await credentialsPromise;
+  const { spreadsheet_id, tokens } = teamInfo;
+
   if (command === "budget") {
-    const totals = await invokeFunction(process.env.getTotalsUrl, teamInfo);
+    const totals = await invokeFunction(
+      process.env.getTotalsUrl,
+      {app_credentials, spreadsheet_id, tokens}
+    );
     const message = responses.reportTotals({totals});
     return messageSlack({response_url, data: message});
   } else if (command === "spend") {
-    const totals = await invokeFunction(process.env.getTotalsUrl, teamInfo);
-    const sendData = {...teamInfo, expense: data};
+    const totals = await invokeFunction(
+      process.env.getTotalsUrl,
+      {app_credentials, spreadsheet_id, tokens}
+    );
     const message = responses.confirmExpense({totals, expense: data});
     return Promise.all([
-      invokeFunction(process.env.addExpenseUrl, sendData),
+      invokeFunction(
+        process.env.addExpenseUrl,
+        {
+          app_credentials,
+          expense: data,
+          spreadsheet_id,
+          tokens,
+        }
+      ),
       messageSlack({response_url, data: message}),
     ]);
   } else {

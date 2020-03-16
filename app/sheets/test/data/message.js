d3256c8d8328b1bd6580355f88316d42978adb96
Andrew Schwartz <andrew.schwartz@brain-power.com>
2020-03-16 19:36:16 -0400

    (/sheets) Remove unused sheets folder now that setup is handled by a deployed function

--- app/sheets/test/data/message.js
+++
@@ -1,8 +0,0 @@
-msg1 = {
-  expense: { amount: '2', category: 'commmute', note: 'train' },
-  slackMessage: { response_url: 'https://hooks.slack.com/commands/TCPPUPVND/952605286982/x8qamk7Xf63bcP60H1pEwcDw', team_id: 'TCPPUPVND', user_name: 'ozydingo', user_id: 'UCPPUPWBB' },
-}
-
-module.exports = {
-  msg1
-}
